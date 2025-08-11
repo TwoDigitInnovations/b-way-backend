95d69e5ec9e01362a7e032fb070225d0ed32b0b1
Alok Dubey <alok73327@gmail.com>
2025-08-11 21:15:18 +0530

    routes with order

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,9 +5,9 @@ const auth = require('@middlewares/authMiddleware');
 
 router.post('/login', login);
 router.post('/register', register);
-router.put('/change-password', auth("ADMIN", "USER", "DRIVER", "DISPATCHER"), changePassword);
-router.put('/update-profile', auth("ADMIN", "USER", "DRIVER", "DISPATCHER"), updateProfile);
-router.get('/user-details', auth("ADMIN", "USER", "DRIVER", "DISPATCHER"), getUserDetails);
+router.put('/change-password', auth("ADMIN", "CLIENT", "HOSPITAL", "DRIVER", "DISPATCHER"), changePassword);
+router.put('/update-profile', auth("ADMIN", "CLIENT", "HOSPITAL", "DRIVER", "DISPATCHER"), updateProfile);
+router.get('/user-details', auth("ADMIN", "CLIENT", "HOSPITAL", "DRIVER", "DISPATCHER"), getUserDetails);
 router.get('/:role', auth("ADMIN"), getUserByType);
 router.get('/all/:role', auth("ADMIN"), getUserList);
 
